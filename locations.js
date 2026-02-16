81a0f38de3cbf439a2b450e7a090673e16810ced
camrbsn-png <camrbsn@gmail.com>
2026-02-16 14:30:38 -0800

    Update locations.js

--- locations.js
+++ locations.js
@@ -523,7 +523,7 @@ const defaultLocations = [
         "lng": -117.6534,
         "addr": "999 San Bernardino Rd, Upland, CA 91786",
         "cat": "Admin",
-        "open": "00:00", 
+        "open": "00:00",
         "close": "23:59",
         "services": ["Emergency", "Surgery", "Inpatient Care"]
     },
@@ -544,6 +544,7 @@ const defaultLocations = [
         "lat": 34.009456,
         "lng": -117.554215,
         "addr": "12510 Micro Dr, Eastvale, CA 91752",
+		"cat": "Admin",
         "open": "08:30",
         "close": "17:30",
         "services": ["Technology Solutions", "Logistics", "Distribution"]
